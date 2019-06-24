a4660a8313c1e3bf4bf701d48e2762a5d6fbb9a9
linfar <andrey.titov@jetbrains.com>
2019-06-24 17:22:32 +0300

    Add sample tests to backend

---
+++ backend/src/test/java/guestbook/GuestBookEntryTest.java
@@ -0,0 +1,21 @@
+package guestbook;
+
+import org.junit.*;
+
+public class GuestBookEntryTest {
+	@Test
+	public void testGetAuthor() {
+	}
+
+	@Test
+	public void testGetMessage() {
+	}
+
+	@Test
+	public void testGetImageUrl() {
+	}
+
+	@Test
+	public void testGetCreatedAt() {
+	}
+}
