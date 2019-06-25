6c5980af64b85c8a463a0c8bfae2baf03aec970d
linfar <andrey.titov@jetbrains.com>
2019-06-25 14:14:38 +0300

    Sample tests and roboDeveloper token for systemTests

---
+++ systemTests/systemTests.sh
@@ -0,0 +1,19 @@
+echo "##teamcity[testStart name='doSomeSystemTest1']"
+echo "##teamcity[testEnd name='doSomeSystemTest1']"
+
+echo "##teamcity[testStart name='doSomeSystemTest2']"
+echo "##teamcity[testEnd name='doSomeSystemTest2']"
+
+echo "##teamcity[testStart name='doSomeSystemTest3']"
+echo "##teamcity[testEnd name='doSomeSystemTest3']"
+
+echo "##teamcity[testStart name='doSomeSystemTest4']"
+echo "##teamcity[testEnd name='doSomeSystemTest4']"
+
+echo "##teamcity[testStart name='doSomeSystemTest5']"
+echo "##teamcity[testEnd name='doSomeSystemTest5']"
+
+echo "##teamcity[testStart name='doSomeSystemTest6']"
+echo "##teamcity[testEnd name='doSomeSystemTest6']"
+
+##roboDevelopersToken
