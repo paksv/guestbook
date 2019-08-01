969d22c341caedf83252a571464c784d20928fdc
sergey.pak <paksv@yandex.ru>
2019-08-01 15:17:30 +0300

    fixed tests script

--- systemTests/systemTests.sh
+++ systemTests/systemTests.sh
@@ -1,19 +1,19 @@
-echo "##teamcity[testStart name='doSomeSystemTest1']"
-echo "##teamcity[testEnd name='doSomeSystemTest1']"
+echo "##teamcity[testStarted name='doSomeSystemTest1']"
+echo "##teamcity[testFinished name='doSomeSystemTest1']"
 
-echo "##teamcity[testStart name='doSomeSystemTest2']"
-echo "##teamcity[testEnd name='doSomeSystemTest2']"
+echo "##teamcity[testStarted name='doSomeSystemTest2']"
+echo "##teamcity[testFinished name='doSomeSystemTest2']"
 
-echo "##teamcity[testStart name='doSomeSystemTest3']"
-echo "##teamcity[testEnd name='doSomeSystemTest3']"
+echo "##teamcity[testStarted name='doSomeSystemTest3']"
+echo "##teamcity[testFinished name='doSomeSystemTest3']"
 
-echo "##teamcity[testStart name='doSomeSystemTest4']"
-echo "##teamcity[testEnd name='doSomeSystemTest4']"
+echo "##teamcity[testStarted name='doSomeSystemTest4']"
+echo "##teamcity[testFinished name='doSomeSystemTest4']"
 
-echo "##teamcity[testStart name='doSomeSystemTest5']"
-echo "##teamcity[testEnd name='doSomeSystemTest5']"
+echo "##teamcity[testStarted name='doSomeSystemTest5']"
+echo "##teamcity[testFinished name='doSomeSystemTest5']"
 
-echo "##teamcity[testStart name='doSomeSystemTest6']"
-echo "##teamcity[testEnd name='doSomeSystemTest6']"
+echo "##teamcity[testStarted name='doSomeSystemTest6']"
+echo "##teamcity[testFinished name='doSomeSystemTest6']"
 
 ##roboDevelopersToken
